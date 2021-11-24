9a3e16ce7aaf2aa09f39679242d13c5d98e7ab72
RabeyaEty01 <etyrabeya01@gmail.com>
2021-11-24 13:23:55 +0600

    explore react sass

---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../Styles/Home.css';
+
+const Home = () => {
+    return (
+        <>
+        <div className="home-page">
+            <h1>Welcome Home</h1>
+            <button>Click Here</button>
+        </div>
+        
+        <button>Click Now</button>
+        </>
+    );
+};
+
+export default Home;
